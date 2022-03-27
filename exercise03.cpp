68826fce695a95ab550e63b2da1a73b97818295c
Muditha Pradeeptha <99239289+IT21235924@users.noreply.github.com>
2022-03-27 06:00:40 +0000

    tutorial 3 done

--- exercise03.cpp
+++ exercise03.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 
 int volume(int height,int width,int length);
+
 // 1. Define a structure called Box
 // have the integer data types Height, Width, Length
 struct box
@@ -17,10 +18,10 @@ int Length;
 // Do not change the main function
 int main() {
     // 2. Create a variable called box1 of the Box structure type
-    struct box box1;
+    box box1;
     // 3. Create a variable called box2 of the Box structure type
-    
-    struct box box2;
+    box box2;
+  
     int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
@@ -30,7 +31,7 @@ int main() {
     cin >> box1.Width;
     cout << "Enter Box 1 Length : ";
     cin >> box1.Length;
-    
+    cout << endl;
     cout << "Enter Box 2 Height : ";
     cin >> box2.Height;
     cout << "Enter Box 2 Width : ";
@@ -51,6 +52,4 @@ int main() {
 int volume(int height,int width,int length)
 {
   return height*width*length;
-     
-  
 }
